3703c8374d55db7dc1769415e2f28cb484b8816a
Ville Takanen <ville.takanen@futurice.com>
2022-12-12 22:07:10 +0200

    feat: installed cinis and proprietary media

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,13 @@ import '@11thdeg/cyan/style.css'
 import '@11thdeg/cyan'
 </script>
 
-<main>
+<cyan-navigation-rail>
+  <cyan-navigation-button noun="page" active/>
+  <cyan-spacer />
+  <cyan-navigation-button noun="fox" label="Pelilauta" />
+  <cyan-navigation-button noun="avatar" label="Kirjaudu" />
+</cyan-navigation-rail>
+
+<main class="bookLayout appContent">
   <slot></slot>
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
-<h1>L&L Hahmonluontikone!</h1>
-<cyan-icon noun="ll" />
-<p>...</p>
+<article class="Column">
+  <div style="text-align:center">
+    <cyan-icon noun="ll" large/>
+  </div>
+  <h1 class="downscaled">Hahmokone</h1>
+  <p>Epävirallinen L&L Hahmokone. Luo hahmon pelaajan kirjan antamassa järjestyksessä, ja täyttää lomakkeen valmiiksi.</p>
+</article>
