49284fc8648d781647634ce62e0fb4c692d21388
Ville Takanen <ville.takanen@futurice.com>
2022-12-13 08:36:56 +0200

    feat: species selector

---
+++ src/lib/client/SpeciesSelect.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+import type { CyanSelect } from '@11thdeg/cyan'
+
+export let species = ''
+
+let speciesOptions = [
+  { value: '-', label: "- Valitse -"},
+  { value: "elf", label: "Haltija" },
+  { value: "human", label: "Ihminen" },
+  { value: "dwarf", label: "Kääpiö" },
+  { value: "dragonling", label: "Louhelainen" },
+  { value: "gnome", label: "Maahinen" },
+  { value: "halfling", label: "Puolituinen"},
+  { value: "changeling", label: 'Vaihdokas'},
+  { value: "orc", label: 'Örkki'}
+]
+
+function handleChange(event: Event) {
+  if (!event.target) return
+  species = (event.target as CyanSelect).value
+}
+</script>
+
+<cyan-select value={species} label="Laji" options={speciesOptions} on:change={handleChange}/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,16 @@
+<script>
+import SpeciesSelect from "../lib/client/SpeciesSelect.svelte"
+
+let species = ''
+</script>
 <article class="Column">
   <div style="text-align:center">
     <cyan-icon noun="ll" large/>
   </div>
   <h1 class="downscaled">Hahmokone</h1>
   <p>Epävirallinen L&L Hahmokone. Luo hahmon pelaajan kirjan antamassa järjestyksessä, ja täyttää lomakkeen valmiiksi.</p>
+  <section class="fieldSet">
+    <SpeciesSelect bind:species={species} />
+    <p>[{species}]</p>
+  </section>
 </article>
